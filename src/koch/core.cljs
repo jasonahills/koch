442935c96f21cbfe4f1357576f85a8f26894560b
Sean Clark Hess <seanhess@gmail.com>
2014-10-15 12:58:44 -0600

    going to get it up online

--- src/koch/core.cljs
+++ src/koch/core.cljs
@@ -34,83 +34,85 @@
 
 (def app-state
   (atom
-    {:contacts
-     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+    {:input-size [20 10]
+     :segments [[0 0] [10 2] [-3 14] [100 100] [80 80]]
+     :message "hello"}))
+
+; (defn contact-view [contact owner]
+;   (reify
+;     om/IRenderState
+;     (render-state [this {:keys [delete]}]
+;       (dom/li nil
+;         (dom/span nil (display-name contact))
+;         (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
+;
+; (defn handle-change [e owner {:keys [text]}]
+;   (let [value (.. e -target -value)]
+;     (if-not (re-find #"[0-9]" value)
+;       (om/set-state! owner :text value)
+;       (om/set-state! owner :text text))))
+;
+; (defn contacts-view [app owner]
+;   (reify
+;     om/IInitState
+;     (init-state [_]
+;       {:delete (chan)
+;        :text ""})
+;     om/IWillMount
+;     (will-mount [_]
+;       (let [delete (om/get-state owner :delete)]
+;         (go (loop []
+;               (let [contact (<! delete)]
+;                 (om/transact! app :contacts
+;                   (fn [xs] (vec (remove #(= contact %) xs))))
+;                 (recur))))))
+;     om/IRenderState
+;     (render-state [this state]
+;       (dom/div nil
+;         (dom/h2 nil "Contact list")
+;         (apply dom/ul nil
+;           (om/build-all contact-view (:contacts app)
+;             {:init-state state}))
+;         (dom/div nil
+;           (dom/input
+;             #js {:type "text" :ref "new-contact" :value (:text state)
+;                  :onChange #(handle-change % owner state)})
+;           (dom/button #js {:onClick #(add-contact app owner)} "Add contact"))))))
 
 
-(defn parse-contact [contact-str]
-  (let [[first middle last :as parts] (string/split contact-str #"\s+")
-        [first last middle] (if (nil? last) [first middle] [first last middle])
-        middle (when middle (string/replace middle "." ""))
-        c (if middle (count middle) 0)]
-    (when (>= (count parts) 2)
-      (cond-> {:first first :last last}
-        (== c 1) (assoc :middle-initial middle)
-        (>= c 2) (assoc :middle middle)))))
+(defn input-point-circle
+  [[x y]]
+  (dom/circle #js {:cx x :cy y :r 4 :stroke "black" :strokeWidth 1 :fill "red"}))
 
-(defn add-contact [app owner]
-  (let [new-contact (-> (om/get-node owner "new-contact")
-                        .-value
-                        parse-contact)]
-    (when new-contact
-      (om/transact! app :contacts #(conj % new-contact))
-      (om/set-state! owner :text ""))))
-
-(defn middle-name [{:keys [middle middle-initial]}]
-  (cond
-    middle (str " " middle)
-    middle-initial (str " " middle-initial ".")))
-
-(defn display-name [{:keys [first last] :as contact}]
-  (str last ", " first (middle-name contact)))
-
-(defn contact-view [contact owner]
+(defn segments-input [segments owner]
   (reify
-    om/IRenderState
-    (render-state [this {:keys [delete]}]
-      (dom/li nil
-        (dom/span nil (display-name contact))
-        (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
-
-(defn handle-change [e owner {:keys [text]}]
-  (let [value (.. e -target -value)]
-    (if-not (re-find #"[0-9]" value)
-      (om/set-state! owner :text value)
-      (om/set-state! owner :text text))))
-
-(defn contacts-view [app owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-      {:delete (chan)
-       :text ""})
-    om/IWillMount
-    (will-mount [_]
-      (let [delete (om/get-state owner :delete)]
-        (go (loop []
-              (let [contact (<! delete)]
-                (om/transact! app :contacts
-                  (fn [xs] (vec (remove #(= contact %) xs))))
-                (recur))))))
     om/IRenderState
     (render-state [this state]
+      (dom/svg #js {:className "segments"}
+        (apply dom/g nil 
+          (map input-point-circle segments))))))
+
+(defn fractal-output [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+      (dom/svg #js {:className "output"} (input-point-circle [100 300])))))
+
+(defn main-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
       (dom/div nil
-        (dom/h2 nil "Contact list")
-        (apply dom/ul nil
-          (om/build-all contact-view (:contacts app)
-            {:init-state state}))
-        (dom/div nil
-          (dom/input
-            #js {:type "text" :ref "new-contact" :value (:text state)
-                 :onChange #(handle-change % owner state)})
-          (dom/button #js {:onClick #(add-contact app owner)} "Add contact"))))))
+        (dom/h2 nil "Koch Fractals")
+        (dom/p nil "So beautiful!")
+        (om/build segments-input (:segments app))
+        (dom/p nil (dom/button nil "Clear Segments"))
+        (dom/p nil
+          (dom/button nil "+")
+          (dom/span nil " # Fractals ")
+          (dom/button nil "-")
+        (dom/p nil
+          (om/build fractal-output nil)))))))
 
-
-
-(om/root contacts-view app-state
+(om/root main-view app-state
   {:target (. js/document (getElementById "app"))})
